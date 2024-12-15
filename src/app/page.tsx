6902a4eb6599e95350166293c31b52519201130a
nanshan <nansshan@163.com>
2024-12-15 21:54:09 +0800

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import SignIn from "@/components/sign-in";
+import UserInfo from "@/components/use-info";
+
+export default function Home() {
+  return (
+    <div>
+      <UserInfo />
+      <SignIn />
+    </div>
+  );
+}
